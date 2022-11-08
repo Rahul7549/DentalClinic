1ad1a7ca60870018ab2132d2e538d2fc77a9e7cc
Rahul7549 <rksharma15700@gmail.com>
2022-11-08 07:14:46 -0500

    First java program

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './modules/dashboard/header/header.component';
+import { HomescreenComponent } from './modules/dashboard/homescreen/homescreen.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'' ,component:HomescreenComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
